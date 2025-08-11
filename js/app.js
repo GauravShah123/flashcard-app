1df83de3a766139048f3c2e373b11a1fb1cbda23
Gaurav Shah <gauravshahstudies@gmail.com>
2025-08-10 23:36:26 -0400

    Reverting Changes

--- js/app.js
+++ js/app.js
@@ -589,4 +589,3 @@ function mulberry32(a) {
     return function () { let t = a += 0x6D2B79F5; t = Math.imul(t ^ t >>> 15, t | 1); t ^= t + Math.imul(t ^ t >>> 7, t | 61); return ((t ^ t >>> 14) >>> 0) / 4294967296; }
 }
 function refreshIcons() { if (window.lucide && lucide.createIcons) try { lucide.createIcons(); } catch (_) { } }
-    
